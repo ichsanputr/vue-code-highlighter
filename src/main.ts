1e0df8d9979691ac62b152117c023d605d9509dc
ichsanputr <ngekode24@gmail.com>
2024-03-02 18:24:00 +0700

    add deployment

--- src/main.ts
+++
@@ -1,4 +0,0 @@
-import VueCodeHighlighter from "./components/VueCodeHighlighter.vue"
-import VueCodeHighlighterMulti from "./components/VueCodeHighlighterMulti.vue"
-
-export { VueCodeHighlighter, VueCodeHighlighterMulti }
